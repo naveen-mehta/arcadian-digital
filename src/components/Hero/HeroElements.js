058169ca27f0ed5e62e819d39a4d93eae75279c5
Naveen <navi.mehta@gmail.com>
2020-12-18 18:29:44 +1100

    Added hero text

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -1,5 +1,42 @@
 import styled from 'styled-components'
+import HeroImage from '../../images/hero.png'
 
 export const HeroContainer = styled.div`
-
+    background-image: url(${HeroImage});
+    background-size: 100%;    
+    position: absolute;
+    width: 100%;
+    height: 75%;
+    background-repeat: no-repeat;
+    left: 0px;
 `
+
+export const HeroText = styled.div`
+    h3 {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 24px;
+        color: #F6F6F6;
+        display: flex;
+        justify-content: center;
+    }
+
+    h1 {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 58px;
+        color: #F6F6F6;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        margin: 0 20%;
+        line-height: 80px;
+    }
+
+    h4 {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 24px;
+        color: #F6F6F6;
+        display: flex;
+        justify-content: center;
+    }
+`
+
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import HeroImage from '../../images/hero.png'
-import { HeroContainer } from './HeroElements'
+import { HeroContainer, HeroText } from './HeroElements'
 
 const Hero = () => {
     return (
         <div>
-            <HeroContainer>
-                <img src={HeroImage} alt=""/>
+            <HeroContainer>          
+                <HeroText>
+                    <h3>Food app</h3>
+                    <h1>Why stay hungry when</h1>
+                    <h1>you can order form Bella Onojie</h1>
+                    <h4>Download the bella onoje’s food app now on</h4>
+                </HeroText>
             </HeroContainer>
+            
         </div>
     )
 }
