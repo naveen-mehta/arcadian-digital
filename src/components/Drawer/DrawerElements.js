c483ab8afd84edcdb3626ac3104d1c89ea46f386
Naveen <navi.mehta@gmail.com>
2020-12-22 19:35:31 +1100

    Added drawer elements.js file

---
+++ src/components/Drawer/DrawerElements.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components'
+import { FaTimes } from 'react-icons/fa'
+import { Link as LinkScroll } from 'react-scroll'
+
+export const DrawerContainer = styled.aside`
+    z-index: 1;
+    width: 100%;
+    height: 1000px;
+    display: grid;
+    background: white;
+    justify-content: center;
+    position: fixed;
+    opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+    top: ${({ isOpen }) => (isOpen ? '0' : '-100%')};
+    transition: all 0.5s ease-in-out;
+`;
+
+export const DrawerCloseContainer = styled.div`
+    position: absolute;
+    top: 1rem;
+    right: 2rem;
+`;
+
+export const DrawerCloseIcon = styled(FaTimes)`
+    color: grey;
+`; 
+
+export const DrawerWrapper = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(14, 70px);
+    text-align: center;
+    align-items: center;
+    margin-top: 5rem;
+    transition: all 0.4s ease-in-out;
+
+    @media screen and (max-width: 480px) {
+        grid-template-rows: repeat(14, 50px);
+        transition: all 0.3s ease-in-out;
+        
+    }
+
+    @media screen and (min-width: 768px) {
+        display: none;
+        grid-template-rows: repeat(14, 50px);
+        transition: all 0.3s ease-in-out;
+    }
+`
+
+export const DrawerOptions = styled.div`
+    transition: all 0.3s ease-in-out;
+`
+
+export const DrawerLink = styled(LinkScroll)`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    text-decoration: none;
+    transition: 0.2s ease-in-out;
+    font-size: 1rem;
+    text-decoration: none;
+    color: grey;
+    list-style: none;
+    padding: 0.7rem; 
+    justify-content: center;
+
+    &:hover {
+        color: black;
+        transition: 0.3 ease-in-out;
+    }
+`
